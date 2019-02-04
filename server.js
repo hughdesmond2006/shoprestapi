f50f01faabee66ad68dec328c8f339cb1a51aec2
hughdesmond2006 <hughdesmond2006@yahoo.ie>
2019-02-04 21:28:45 +0700

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('app');
+
+//if default not set then set to port 3000
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
