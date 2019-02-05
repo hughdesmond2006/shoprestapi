53e333e0ccf226233c84b15e6bee749613d6e7b9
hughdesmond2006 <hughdesmond2006@yahoo.ie>
2019-02-05 17:25:22 +0700

    Swapped dummy data with atlas mongo db. products now fully working, can GET, POST, PATCH and DELETE. Responses setup with removed boiler plate data and links to next action

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,21 +1,86 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handling GET reqs for /products'
-    });
+    Product.find()
+        .select('name price _id')
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return{
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request:{
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            };
+
+            console.log(response);
+
+            //if returns empty array, then its an error??
+            /**if(docs.length >= 0){
+                res.status(200).json(docs)
+            } else {
+                res.status(404).json({
+                    message: 'no valid entry found for this ID'
+                });
+            }**/
+
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(200).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'handling POST requests for /products'
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
     });
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'created product!!',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request:{
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.get('/:productId', (req, res, next) =>{
     const id = req.params.productId;
-    if(id === "special"){
+
+    //Dummy response
+    /**if(id === "special"){
         res.status(200).json({
             message: 'You got the special ID',
             id: id
@@ -24,19 +89,83 @@ router.get('/:productId', (req, res, next) =>{
         res.status(200).json({
             message: 'You passed an ID'
         });
-    }
+    }**/
+
+    Product.findById(id)
+        .select('name price _id')
+        .exec()
+        .then(doc => {
+            console.log("from db:" + doc);
+            if(doc){
+                res.status(200).json({
+                    product: doc,
+                    request:{
+                        type: 'GET',
+                        description: 'Get All Products',
+                        url: 'http://localhost:3000/products/'
+                    }
+                })
+            } else {
+                res.status(404).json({
+                    message: 'no valid entry found for this ID'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
 });
 
 router.patch('/:productId', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Updated product'
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id}, {$set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Product Updated!!',
+                request:{
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
 });
 
 router.delete('/:productId', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Deleted product'
-    });
+    const id = req.params.productId;
+    Product.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Product Deleted!!',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products/',
+                    body: {
+                        name: 'String',
+                        price: 'Number'
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        })
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');   //this package is for logging get/post requests on the server
+const bparser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect(
+    'mongodb://hugh:' +
+    process.env.MONGO_ATLAS_PW +
+    '@scoopycluster-shard-00-00-qdudo.mongodb.net:27017,scoopycluster-shard-00-01-qdudo.mongodb.net:27017,scoopycluster-shard-00-02-qdudo.mongodb.net:27017/test?ssl=true&replicaSet=scoopycluster-shard-0&authSource=admin&retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
+mongoose.Promise = global.Promise;
+
 app.use(morgan('dev'));
+app.use(bparser.urlencoded({extended: false}));
+app.use(bparser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //routes that handle requests
 app.use('/products', productRoutes);
